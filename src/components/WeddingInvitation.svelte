43b5f99486991ebb7667b7698f478cbdc3e3b3ff
rizal suryawan <rizalsuryawan86@gmail.com>
2024-04-14 02:24:14 +0700

    feat: add wedding section date

--- src/components/WeddingInvitation.svelte
+++ src/components/WeddingInvitation.svelte
@@ -1,17 +1,23 @@
 <script>
-	import hero from '$lib/images/hero.jpeg';
+	// import hero from '$lib/images/hero.jpeg';
+	// import { flip } from 'svelte/animate';
 </script>
 
-<section
-	class="flex flex-col h-screen w-screen bg-center bg-cover backdrop-brightness-50 bg-white/30"
-	style="background-image: url('{hero}')"
->
-	<div class=" flex flex-col items-center justify-end text-center h-full pb-10">
+<section class="flex flex-col h-screen w-screen bg-center bg-cover bg-no-repeat hero">
+	<div class=" flex flex-col items-center justify-start pt-12 text-center h-full pb-10 text-white">
 		<h3 class=" text-xl capitalize italic font-tangerine">the wedding of</h3>
-		<div class=" flex justify-center text-6xl items-center gap-x-5 font-sedan">
+		<div class=" flex justify-center text-6xl items-center gap-x-5 pb-4">
 			<p class="py-4 text-3xl font-bold">Raffi</p>
 			&
 			<p class="py-4 text-3xl font-bold">Inah</p>
 		</div>
+		<span>04 mei 2024</span>
 	</div>
 </section>
+
+<style>
+	.hero {
+		background-image: linear-gradient(to bottom, #000000 0%, #ffffff80 100%),
+			url($lib/images/hero.jpeg);
+	}
+</style>

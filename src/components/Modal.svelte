b8ee9d88c485084d887e8c7f8d1818294aa296ce
rizal suryawan <rizalsuryawan86@gmail.com>
2024-04-14 00:30:47 +0700

    feat: add modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,31 @@
+<script>
+	let isModalOpen = true;
+</script>
+
+<section class=" ">
+	<div class="modal" class:modal-open={isModalOpen}>
+		<div class="modal-box bg-transparent text-center text-white">
+			<h3 class=" text-xl capitalize">the wedding of</h3>
+			<div class=" flex justify-center text-6xl items-center gap-x-5">
+				<p class="py-4 text-3xl">Rafiudin <br /><span class=" text-xl"> ( Raffi ) </span></p>
+				&
+				<p class="py-4 text-3xl">Jumainah <br /><span class=" text-xl"> ( Inah ) </span></p>
+			</div>
+			<div class="modal-action justify-center items-center">
+				<button class="btn btn-accent" on:click={() => (isModalOpen = false)}>Buka Undangan</button>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+	.modal:not(dialog:not(.modal-open)),
+	.modal::backdrop {
+		background-image: url('https://vasiota.com/wp-content/uploads/2022/11/download-8.jpeg');
+		background-color: #fff;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+		animation: modal-pop 0.2s ease-out;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the 
-  documentation.
-</p>
+<script>
+	import '../app.css';
+	import Modal from '../components/Modal.svelte';
+	import WeddingInvitation from '../components/WeddingInvitation.svelte';
+</script>
+
+<svelte:head>
+	<title>Raffi & Inah</title>
+	<meta name="description" content="Wedding Invitation" />
+</svelte:head>
+
+<section class=" flex flex-col min-h-screen justify-start items-center bg-white">
+	<!-- Modal start -->
+	<Modal />
+	<!-- Modal end -->
+	<!-- Wedding invitation start -->
+	<WeddingInvitation />
+	<!-- Wedding invitation end -->
+</section>

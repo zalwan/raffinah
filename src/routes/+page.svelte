2036d30284e6ac4da7d1744a2e9644ef9c2b7cdd
rizal suryawan <rizalsuryawan86@gmail.com>
2024-04-14 05:00:01 +0700

    feat: add maps. date, parent, all ornament

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import '../app.css';
 	import Modal from '../components/Modal.svelte';
+	import ParentInvitation from '../components/ParentInvitation.svelte';
 	import WeddingInvitation from '../components/WeddingInvitation.svelte';
+	import Date from '../components/Date.svelte';
+	import Map from '../components/Map.svelte';
 </script>
 
 <svelte:head>
@@ -9,11 +12,20 @@
 	<meta name="description" content="Wedding Invitation" />
 </svelte:head>
 
-<section class=" flex flex-col min-h-screen justify-start items-center bg-white">
+<section class=" flex flex-col min-h-screen justify-start items-center bg-black">
 	<!-- Modal start -->
 	<Modal />
 	<!-- Modal end -->
 	<!-- Wedding invitation start -->
 	<WeddingInvitation />
 	<!-- Wedding invitation end -->
+	<!-- Parent start -->
+	<ParentInvitation />
+	<!-- Parent end -->
+	<!-- Date start -->
+	<Date />
+	<!-- Date end -->
+	<!-- Map start -->
+	<Map />
+	<!-- Map end -->
 </section>
